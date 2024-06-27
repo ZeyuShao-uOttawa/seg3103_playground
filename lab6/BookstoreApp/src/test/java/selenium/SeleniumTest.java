c8da7711bc571cd909eb0cf2f38066ebdfa2ecad
Zeyu Shao <zeyu.shao@hotmail.com>
2024-06-26 23:51:41 -0400

    Added one additional selenium web driver test

---
+++ lab6/BookstoreApp/src/test/java/selenium/SeleniumTest.java
@@ -0,0 +1,64 @@
+package selenium;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class SeleniumTest {
+
+  static Process server;
+  private WebDriver driver;
+
+  @BeforeAll
+  public static void setUpBeforeClass() throws Exception {
+    ProcessBuilder pb = new ProcessBuilder("java", "-jar", "bookstore5.jar");
+    server = pb.start();
+  }
+
+  @BeforeEach
+  void setUp() {
+    // Pick your browser
+    // driver = new FirefoxDriver();
+    // driver = new SafariDriver();
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get("http://localhost:8080/admin");
+    // wait to make sure Selenium is done loading the page
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("title")));
+  }
+
+  @AfterEach
+  public void tearDown() {
+    driver.close();
+  }
+
+  @AfterAll
+  public static void tearDownAfterClass() throws Exception {
+    server.destroy();
+  }
+
+  @Test
+  void test1() {
+    WebElement element = driver.findElement(By.id("loginBtn"));
+    String expected = "Sign in";
+    String actual = element.getAttribute("value");
+    assertEquals(expected, actual);
+  }
+}
