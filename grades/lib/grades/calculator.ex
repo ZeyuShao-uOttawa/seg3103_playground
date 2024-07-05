3d1303e8e27a6dc07917f0b0dbe468d430e8ea20
Zeyu Shao <zeyu.shao@hotmail.com>
2024-07-04 23:28:43 -0400

    2.2 Refactored dupe code into failed_to_participate?() function

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -30,7 +30,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -68,7 +68,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -95,5 +95,9 @@ defmodule Grades.Calculator do
       else
         Enum.sum(homework) / Enum.count(homework)
       end
+  end
+
+  def failed_to_participate?(avg_homework, avg_exams, num_labs) do
+    avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
   end
 end
